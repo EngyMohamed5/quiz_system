082511e5ca44200743c5ab71310cc3e6e8c6c1fc
EngyMohamed5 <engymohamedalratmy@gmail.com>
2024-09-22 15:35:01 +0300

    set quiz and topic relation

--- app/Models/Quiz.php
+++ app/Models/Quiz.php
@@ -8,4 +8,9 @@ use Illuminate\Database\Eloquent\Model;
 class Quiz extends Model
 {
     use HasFactory;
+    
+    public function topic()
+    {
+        return $this->belongsTo(Topic::class);
+    }
 }
