383474bc8fa203a35847c52a00e04a6735bb7898
Mennatu-allahNabil <100155102+Mennatu-allahNabil@users.noreply.github.com>
2024-09-30 19:41:29 +0300

    resolved errors

--- app/Http/Controllers/QuizController.php
+++ app/Http/Controllers/QuizController.php
@@ -1,8 +1,6 @@
 <?php
 
 namespace App\Http\Controllers;
-
-use App\Mail\AnswerMail;
 use App\Models\Option;
 use App\Models\Question;
 use App\Models\PerformanceHistory;
@@ -13,7 +11,6 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use App\Traits\Uploadimage;
 use App\Traits\CheckFile;
-use Illuminate\Support\Facades\Mail;
 use RealRashid\SweetAlert\Facades\Alert;
 
 class QuizController extends Controller
@@ -32,61 +29,64 @@ class QuizController extends Controller
 
     public function store(Request $request)
     {
-        dd($request->questions);
 
-        try {
-            $request->validate([
-                "title" => "required|string|unique:quizzes",
-                "description" => "required|string",
-                'image' => 'nullable|image|mimes:jpeg,png,jpg,gif',
-                "quiz_type" => "required",
-                "time_limit" => "required|integer",
-                "topic_id" => "required|integer",
-                "questions" => "required|array|min:1",
-                "questions.*.text" => "required|string",
-                "questions.*.type" => "required|string",
-                "questions.*.image" => "nullable|image|mimes:jpeg,png,jpg,gif",
-                "questions.*.options" => "required|array|min:2",
-                "questions.*.options.*" => "required|string",
-                "questions.*.is_correct_number" => "required|integer|min:1|max:4",
-                "created_by" => "required|integer",
-            ]);
-            DB::transaction(function () use ($request) {
-                $quiz_data = [
-                    "title" => $request->title,
-                    "description" => $request->description,
-                    "quiz_type" => $request->quiz_type,
-                    "time_limit" => $request->time_limit,
-                    "created_by" => $request->created_by,
-                    "topic_id" => $request->topic_id,
-                ];
-                if ($this->checkFile($request, 'image')) {
-                    $quiz_data['image'] = $this->uploadImage($request, 'image', 'Quizzes_images');;
-                }
+      try{
+          $request->validate([
+              "title" => "required|string|unique:quizzes",
+              "description" => "required|string",
+              'image' => 'nullable|image|mimes:jpeg,png,jpg,gif',
+              "quiz_type" => "required",
+              "time_limit" => "required|integer",
+              "topic_id" => "required|integer",
+              "questions" => "required|array|min:1",
+              "questions.*.text" => "required|string",
+              "questions.*.type" => "required|string",
+              "questions.*.image" => "nullable|image|mimes:jpeg,png,jpg,gif",
+              "questions.*.options" => "required|array|min:2",
+              "questions.*.options.*" => "required|string",
+              "questions.*.is_correct_number" => "required|integer|min:1|max:4",
+              "created_by" => "required|integer",
+          ]);
 
-                $quiz = Quiz::create($quiz_data);
+          DB::transaction(function () use ($request) {
+              $quiz_data=[
+                  "title"=>$request->title,
+                  "description"=>$request->description,
+                  "quiz_type"=>$request->quize_type,
+                  "time_limit"=>$request->time_limit,
+                  "created_by"=>$request->created_by,
+                  "topic_id"=>$request->topic_id,
+              ];
+              if($this->checkFile($request,'image')){
+                  $quiz_data['image']=$this->uploadImage($request, 'image', 'Quizzes_images');
+              }
 
-                foreach ($request->questions as $question_to_store) {
-                    //                  dd($question_to_store);
-                    $question_data = [
-                        "question_text" => $question_to_store['text'],
-                        "question_type" => $question_to_store['type'],
-                    ];
-                    if (array_key_exists('image', $question_to_store)) {
-                        $question_data['image'] = $this->uploadImage($request, 'image', 'Questions_images');
-                    }
-                    $question = $quiz->questions()->create($question_data);
-                    foreach ($question_to_store['options'] as $index => $option) {
-                        $question->options()->create([
+              $quiz= Quiz::create($quiz_data);
+
+              foreach ($request->questions as $question_to_store) {
+//                  dd($question_to_store);
+                  $question_data=[
+                      "question_text"=>$question_to_store['text'],
+                      "question_type"=>$question_to_store['type'],
+                  ];
+                  if(array_key_exists('image',$question_to_store)){
+                      $question_data['image']=$this->uploadImage($request, 'image', 'Questions_images');
+                  }
+                  $question = $quiz->questions()->create($question_data);
+                  foreach ($question_to_store['options'] as $index => $option) {
+                      $question->options()->create([
                             'option_text' => $option,
                             'is_correct' => ($question_to_store['is_correct_number'] - 1) === $index ? 1 : 0,
-                        ]);
-                    }
-                }
-            });
-        } catch (\Exception $e) {
-            return $e->getMessage();
-        }
+                      ]);
+                  }
+              }
+          });
+      }catch (\Exception $e){
+          return $e->getMessage();
+      }
+
+
+
     }
 
     public function submitQuiz(Request $request)
@@ -135,8 +135,8 @@ class QuizController extends Controller
         $percentageScore = ($totalQuestions > 0) ? ($score / $totalQuestions) * 100 : 0;
 
         $latestAttempt = PerformanceHistory::where('user_id', auth()->id())
-            ->where('quiz_id', $quizId)
-            ->max('attempt_number');
+        ->where('quiz_id', $quizId)
+        ->max('attempt_number');
         $newAttemptNumber = $latestAttempt ? $latestAttempt + 1 : 1;
 
         PerformanceHistory::create([
@@ -146,14 +146,6 @@ class QuizController extends Controller
             'attempt_number' => $newAttemptNumber,
         ]);
 
-        $quiz = Quiz::select('title', 'quiz_type')->find($quizId);
-
-        if ($quiz->quiz_type == "once_attempt") {
-            $useranswer = Answer::where('user_id', auth()->id())
-                ->where('quiz_id', $quizId)
-                ->get();
-            Mail::to(auth()->user()->email)->send(new AnswerMail($questions, $useranswer, $quiz->title));
-        }
 
         return redirect()->route('score.view', [
             'score' => $score,
@@ -176,10 +168,10 @@ class QuizController extends Controller
             'performance_histories.attempt_number',
 
         )
-            ->join('users', 'performance_histories.user_id', '=', 'users.id')
-            ->join('quizzes', 'performance_histories.quiz_id', '=', 'quizzes.id')
-            ->where('performance_histories.user_id', $userId)
-            ->get();
+        ->join('users', 'performance_histories.user_id', '=', 'users.id')
+        ->join('quizzes', 'performance_histories.quiz_id', '=', 'quizzes.id')
+        ->where('performance_histories.user_id', $userId)
+        ->get();
 
 
         $score = $request->input('score');
@@ -191,20 +183,42 @@ class QuizController extends Controller
 
 
 
-    public function showdata(Request $request)
-    {
-        $userId = auth()->id();
+public function showdata(Request $request)
+{
+    $userId = auth()->id();
 
 
-        $results = PerformanceHistory::select('performance_histories.user_id', 'users.name', 'performance_histories.score')
-            ->join('users', 'performance_histories.user_id', '=', 'users.id')
-            ->get();
+    $results = PerformanceHistory::select('performance_histories.user_id', 'users.name', 'performance_histories.score')
+        ->join('users', 'performance_histories.user_id', '=', 'users.id')
+        ->get();
 
 
-        $score = $request->input('score');
-        $total = $request->input('total');
-        $percentage = $request->input('percentage');
+    $score = $request->input('score');
+    $total = $request->input('total');
+    $percentage = $request->input('percentage');
 
-        return view('quiz.showresults', compact('score', 'total', 'percentage', 'results', 'userId'));
-    }
+    return view('quiz.showresults', compact('score', 'total', 'percentage', 'results', 'userId'));
+
+
+
+}
+ // here i show quiz and delete
+public function index()
+{
+    $quizzes = Quiz::all();
+    return view('dashboard.quiz.index', compact('quizzes'));
+}
+public function show(Quiz $quiz)
+{
+    $quiz->load('questions.options');
+    return view('dashboard.quiz.show', compact('quiz'));
+}
+
+
+public function destroy(Quiz $quiz)
+{
+    $quiz->delete();
+    return redirect()->route('quiz.index')->with('success', 'Quiz deleted successfully.');
+}
+
 }
--- routes/web.php
+++ routes/web.php
@@ -5,8 +5,7 @@ use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\TopicController;
 use App\Http\Controllers\QuizController;
-use App\Mail\AnswerMail;
-use Illuminate\Support\Facades\Mail;
+use App\Http\Controllers\QuestionController;
 use Illuminate\Support\Facades\Route;
 use SebastianBergmann\CodeCoverage\Report\Html\Dashboard;
 
@@ -24,7 +23,6 @@ use SebastianBergmann\CodeCoverage\Report\Html\Dashboard;
 Route::get('/', function () {
     return view('dashboard');
 });
-
 
 Route::get('/dashboard', function () {
     return view('dashboard');
@@ -59,6 +57,22 @@ Route::get('admin/createQuiz', [AdminController::class, 'createQuizPage'])
     ->name('admin.CreateQuiz')->middleware('admin');
 Route::post("admin/storeQuiz", [QuizController::class, 'store'])->name('quiz.store')->middleware('admin');
 
+
+
+/*....................................................................... */
+//quiz admin update  delete
+Route::get('admin/quizzes', [QuizController::class, 'index'])->name('quiz.index');
+Route::delete('admin/quizzes/{quiz}', [QuizController::class, 'destroy'])->name('quiz.delete');
+Route::get('admin/quizzes/{quiz}/questions', [QuestionController::class, 'index'])->name('questions.index');
+Route::resource('admin.quizzes.questions', QuestionController::class)->parameters([
+    'questions' => 'question'
+]);
+Route::get('admin/quizzes/{quiz}/questions/{question}', [QuestionController::class, 'show'])->name('questions.show'); // Show a specific question
+Route::get('admin/quizzes/{quiz}/questions/{question}/edit', [QuestionController::class, 'edit'])->name('questions.edit');
+Route::put('admin/quizzes/{quiz}/questions/{question}', [QuestionController::class, 'update'])->name('questions.update');
+Route::delete('quizzes/{quiz}/questions/{question}', [QuestionController::class, 'destroy'])->name('questions.destroy');
+
+/*....................................................................... */
 
 
 Route::middleware('auth')->group(function () {
